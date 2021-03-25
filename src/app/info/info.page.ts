5374ead9670ca80d1467d66a54da897ac2481b91
Lucas Dalda <lucasdalda@gmail.com>
2021-03-25 00:22:31 -0300

    add pack Anime.js

--- src/app/info/info.page.ts
+++ src/app/info/info.page.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.page.html',
   styleUrls: ['./info.page.scss'],
+  
 })
 export class InfoPage implements OnInit {
 
