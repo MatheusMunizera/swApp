fd30273211fe91ff15c278e394db24e983e52e97
Henrique <hmendesdebrito@outlook.com>
2021-04-06 19:09:15 -0300

    teste telas

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -25,7 +25,58 @@ interface CharacterNumber{
 
 export class ListPage implements OnInit {
 
-  public charater: Character[] = []
+  public charater = [ 
+    {
+      name: 'Luke Skywalker',
+      height: '172',
+      mass: '77',
+      hair: 'blond',
+      skin: 'fair',
+      eye: 'blue',
+      birth: '19BBY',
+      gender: 'male'
+    },
+    {
+      name: 'C-3PO',
+      height: '167',
+      mass: '75',
+      hair: 'n/a',
+      skin: 'gold',
+      eye: 'yellow',
+      birth: '112BBY',
+      gender: 'n/a'
+    },
+    {
+      name: 'R2-D2',
+      height: '96',
+      mass: '32',
+      hair: 'n/a',
+      skin: 'white, blue',
+      eye: 'red',
+      birth: '33BBY',
+      gender: 'n/a'
+    },
+    {
+      name: 'Darth Vader',
+      height: '202',
+      mass: '136',
+      hair: 'none',
+      skin: 'white',
+      eye: 'yellow',
+      birth: '41.9BBY',
+      gender: 'male'
+    },
+    {
+      name: 'Leia Organa',
+      height: '150',
+      mass: '49',
+      hair: 'brown',
+      skin: 'light',
+      eye: 'brown',
+      birth: '19BBY',
+      gender: 'female'
+    }
+  ]
   public num: CharacterNumber[] = []
   public pers: Variable[] = [];
 
