5ac8ef791c4db22050568c315e3893af9a492120
Lucas Dalda <lucasdalda@gmail.com>
2021-04-06 23:20:19 -0300

    add segmentCard/styling

--- src/app/info/info.page.ts
+++ src/app/info/info.page.ts
@@ -1,21 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { ElementRef, Component, OnInit, ViewChild } from '@angular/core';
 import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.page.html',
   styleUrls: ['./info.page.scss'],
-  
+
 })
 export class InfoPage implements OnInit {
 
-  
-
-
-
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
+  segmentCard = "Tech"
+ 
+
+  mudarCor() {
+    switch (this.segmentCard) {
+      case 'Tech':
+        document.getElementById('Tech').style.cssText =
+          '-webkit-text-stroke-color: #000000';
+        document.getElementById('Prod').style.cssText =
+          '-webkit-text-stroke-color: #ffc500';
+        document.getElementById('Pol').style.cssText =
+          '-webkit-text-stroke-color: #ffc500';
+        break;
+      case 'Prod':
+        document.getElementById('Tech').style.cssText =
+          '-webkit-text-stroke-color: #ffc500';
+        document.getElementById('Prod').style.cssText =
+          '-webkit-text-stroke-color: #000000';
+        document.getElementById('Pol').style.cssText =
+          '-webkit-text-stroke-color: #ffc500';
+          break;
+      case 'Pol':
+        document.getElementById('Tech').style.cssText =
+        '-webkit-text-stroke-color: #ffc500';
+      document.getElementById('Prod').style.cssText =
+        '-webkit-text-stroke-color: #ffc500';
+      document.getElementById('Pol').style.cssText =
+        '-webkit-text-stroke-color: #000000';
+        break;
+    }
+  }
 }
