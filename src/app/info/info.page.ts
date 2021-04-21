e7832eb17f82620734207d5eec7453b2bad4a91b
Lucas Dalda <lucasdalda@gmail.com>
2021-04-21 00:27:18 -0300

    finalBuild_teste

--- src/app/info/info.page.ts
+++ src/app/info/info.page.ts
@@ -15,33 +15,5 @@ export class InfoPage implements OnInit {
 
   segmentCard = ""
 
-  mudarCor() {
-    switch (this.segmentCard) {
-      case 'Tech':
-        document.getElementById('Tech').style.cssText =
-          '-webkit-text-stroke-color: #000000';
-          '-webkit-text-stroke-color: #ffc500';
-        document.getElementById('Prod').style.cssText =
-          '-webkit-text-stroke-color: #ffc500';
-        document.getElementById('Pol').style.cssText =
-          '-webkit-text-stroke-color: #ffc500';
-        break;
-      case 'Prod':
-        document.getElementById('Tech').style.cssText =
-          '-webkit-text-stroke-color: #ffc500';
-        document.getElementById('Prod').style.cssText =
-          '-webkit-text-stroke-color: #000000';
-        document.getElementById('Pol').style.cssText =
-          '-webkit-text-stroke-color: #ffc500';
-          break;
-      case 'Pol':
-        document.getElementById('Tech').style.cssText =
-        '-webkit-text-stroke-color: #ffc500';
-      document.getElementById('Prod').style.cssText =
-        '-webkit-text-stroke-color: #ffc500';
-      document.getElementById('Pol').style.cssText =
-        '-webkit-text-stroke-color: #000000';
-        break;
-    }
-  }
+  
 }
