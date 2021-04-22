3b999e61d3c9ed0548cd361cf03f97ab7bb2bcc7
Daniel.Alves <daniel.alves17@outlook.com>
2021-04-22 20:03:54 -0300

    Correção de erro e layout sideMenu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: 'random',
@@ -27,7 +27,14 @@ const routes: Routes = [
     path: 'search',
     loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule)
   },
-
+  {
+    path: 'filmes',
+    loadChildren: () => import('./filmes/filmes.module').then( m => m.FilmesPageModule)
+  },
+  {
+    path: 'series',
+    loadChildren: () => import('./series/series.module').then( m => m.SeriesPageModule)
+  }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'home', url: '/folder/Inbox'},
-    { title: 'listas', url: '/list'},
-    { title: 'random', url: '/random'},
-    { title: 'Quizz', url: '/quizz'},
+    { title: 'Home', url: '/home', icon: "home"},
+    { title: 'Filmes', url: '/filmes', icon: "film"},
+    { title: 'Series', url: '/series', icon: "ticket"},
+    { title: 'Lista de personagens', url: '/list', icon: "list"},
+    { title: 'Random', url: '/random', icon:"telescope"},
+    { title: 'Quizz', url: '/quizz',  icon: "document-text"},
 
+
+  ];
+  public labels = [
+    { title: 'O Projeto', url: 'https://github.com/MatheusMunizera/swApp', icon: "logo-github"},
+    { title: 'API', url: 'https://github.com/MatheusMunizera/starwars-api', icon: "logo-github"},
+    { title: 'Youtube', url: 'https://www.youtube.com/', icon: "logo-youtube"},
+    { title: 'Discord', url: 'https://discord.gg/tghrPuD', icon: "logo-discord"},
   ];
   constructor() {}
 }
