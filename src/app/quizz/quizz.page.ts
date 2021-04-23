57abf0fd83f43ae24fffc204dab4771b861a5f89
Pedro <Pedro@MacBook-Air-de-Pedro.local>
2021-04-22 21:13:04 -0300

    construção do design final

--- src/app/quizz/quizz.page.ts
+++ src/app/quizz/quizz.page.ts
@@ -32,7 +32,7 @@ export class QuizzPage implements OnInit {
   public questions = [{
     text: "O primeiro filme da saga foi lançado em 1978?",
     answers:
-      ["Não, em 1977",
+      ["Não",
         "Sim"
       ]
 
@@ -40,32 +40,32 @@ export class QuizzPage implements OnInit {
   {
     text: "O rival de Anakin Skywalker na corrida de pods era Watto?",
     answers:
-      ["Não, o nome do rival de Anakin era Sebulba.",
-        "Sim, Watto correu contra Anakin em Boonta Eve."
+      ["Mentira",
+        "Verdade"
       ]
 
   },
   {
     text: "O número da cela onde Leia ficou presa na Estrela da Morte era 2167?",
     answers:
-      ["Não, Luke salvou a princesa na cela 2187.",
-        "Sim, foi aí que Luke Skywalker resgatou a princesa."
+      ["Não, era 2187.",
+        "Sim"
       ]
 
   },
   {
     text: "O planeta natal de Han Solo é Bespin?",
     answers:
-      ["Não, Han Solo nasceu em Corellia, planeta conhecido por suas fábricas de naves.",
-        "Sim, Han Solo nasceu em Bespin, o mesmo planeta onde Lando Calrissian administra uma usina de gás Tibanna no Episódio V."
+      ["Não, é Corellia.",
+        "Verdade"
       ]
 
   },
   {
     text: "Em seu duelo com Anakin, Obi-Wan corta todos os quatro membros de seu ex-pupilo?",
     answers:
-      ["Errado, Obi-Wan poupa um dos braços de Anakin.",
-        "Correto, Obi-Wan deixa Anakin sem nenhum membro, sendo substituídos por próteses robóticas."
+      ["Errado",
+        "Correto"
       ]
 
   },
@@ -78,19 +78,23 @@ export class QuizzPage implements OnInit {
 
   },
   {
-    text: "Uma vez, Mestre Yoda, disse: ",
+    text: "É verdade que: Uma vez, Mestre Yoda, disse: Você ainda tem muito para aprender ?",
     answers:
-      ["Você ainda tem muito para aprender",
-        "Você é capaz, confia pai"
+      ["Verdadeira",
+        "Nunca disse"
       ]
 
   },
   {
     text: "C3PO é fluente em mais de oito milhões de formas de comunicação?",
     answers:
-      ["Não, C3PO compreende seis milhões de formas de comunicação.",
-        "Sim, o dróide de protocolo é fluente em oito milhões de formas de comunicação, inclusive Sith."
+      ["Não",
+        "Sim"
       ]
+
+
+
+
 
   }
   ]
