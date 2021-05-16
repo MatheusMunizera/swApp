ac304f5d128cb9023474882df9be161cefa968b5
Henrique <hmendesdebrito@outlook.com>
2021-05-16 16:11:21 -0300

    routing info

--- src/app/info/info.page.ts
+++ src/app/info/info.page.ts
@@ -2,6 +2,8 @@ import { ElementRef, Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SwAppService } from '../services/sw-app.service';
 import { HttpClient } from '@angular/common/http';
+import { resolve } from 'q';
+import { Variable } from '@angular/compiler/src/render3/r3_ast';
 
 @Component({
   selector: 'app-info',
@@ -12,20 +14,15 @@ import { HttpClient } from '@angular/common/http';
 export class InfoPage implements OnInit {
    readonly apiUrl = "https://matheusmunizera.github.io/starwars-api/api/"
    segmentCard = ""
+   current: Variable;
 
-  constructor(private route: ActivatedRoute, private  http: HttpClient ) {
+  constructor(private route: ActivatedRoute, private  http: HttpClient, private swService: SwAppService ) {
     const user = route.snapshot.paramMap.get('id');
-
-    this.loadInfo(user)
-   }
+    swService.loadInfo(this.apiUrl + user + ".json").then(data => {
+      this.current = JSON.parse(JSON.stringify(data));
+      console.log(this.current);
+    });
+  }
 
   ngOnInit() { }
-
- 
-
-   private loadInfo (item: String) {
-    
-
-   }
-  
 }
