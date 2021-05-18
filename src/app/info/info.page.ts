0fce35f6c11db2232d3d55bfc0fe122a404dcc65
Lucas Dalda <lucasdalda@gmail.com>
2021-05-17 21:32:41 -0300

    Get info from service

--- src/app/info/info.page.ts
+++ src/app/info/info.page.ts
@@ -15,24 +15,35 @@ export class InfoPage implements OnInit {
     const user = route.snapshot.paramMap.get('id');
     console.log(user);
     this.checkType(user);
+
+    let x = this.typeItem
+    console.log(this.typeItem)
   }
 
+
   ngOnInit() {}
+
+  public typeItem
+  
 
   public currentItem;
 
   private loadInfo(type: string, id) {
     switch (type) {
       case 'characters':
+        this.typeItem = 'characters';
         this.currentItem = this.swService.characterList[id - 1];
         break;
       case 'planets':
+        this.typeItem = 'planets';
         this.currentItem = this.swService.planetsList[id - 1];
         break;
       case 'species':
+        this.typeItem = 'species';
         this.currentItem = this.swService.speciesList[id - 1];
         break;
       case 'vehicles':
+        this.typeItem = 'vehicles';
         this.currentItem = this.swService.vehiclesList[id - 1];
         break;
     }
@@ -45,4 +56,5 @@ export class InfoPage implements OnInit {
 
     this.loadInfo(type, id);
   }
+
 }
